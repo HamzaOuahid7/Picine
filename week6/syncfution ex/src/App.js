0ee96cc964dd76529213c85eceeab0ba19f83bc5
HamzaOuahid7 <hamza.ouahid@keiken-digital.com>
2023-03-22 17:56:42 +0000

    syncfution

---
+++ week6/syncfution ex/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
+import { data } from './datasource';
+function App() {
+  return (
+    <div ><GridComponent dataSource={data}>
+      <ColumnsDirective>
+            <ColumnDirective field='OrderID' width='100' textAlign="Right"/>
+            <ColumnDirective field='CustomerID' width='100'/>
+            <ColumnDirective field='EmployeeID' width='100' textAlign="Right"/>
+            <ColumnDirective field='Freight' width='100' format="C2" textAlign="Right"/>
+            <ColumnDirective field='ShipCountry' width='100'/>
+        </ColumnsDirective>
+    </GridComponent>
+
+    </div>
+  );
+}
+
+export default App;
